2d33a6ce1b7269f3cf00a53b8572fed69fd1e3b4
André Vieira - IVIA <sgavsnake@gmail.com>
2019-11-06 00:02:31 -0300

    test realizado

--- tests/test_info_ponto.py
+++ tests/test_info_ponto.py
@@ -1,11 +1,7 @@
 import pytest
 
-from api.routers.Ponto import colaborador_db
 from tests.mockData import col1, col2
 from api.models.PontoModel import Ponto
-from api.models.ColaboradorModel import Colaborador
-from utils.time_utils import date_format
-import pendulum
 
 
 @pytest.fixture()
@@ -16,16 +12,15 @@ def web():
 
 
 def test_compute_hours():
-    ponto = Ponto(colaborador_id=col1.colabaorador_id)
-    ponto.registros_ES = [{'entrada': '00:00:00', 'saida': '10:00:00'}]
-
-    #TODO continuar e computar as horas it s easy
-
+    ponto = Ponto(colaborador_id='12345678')
     ponto.criar_registro_ES()
-    ponto.registrar_entrada_in("10:00:00")
-    ponto.registrar_saida_in("11:00:00")
+    ponto.registrar_entrada_in('00:00:00')
+    ponto.registrar_saida_in('10:00:00')
 
-    res = ponto.calcular_horas_mes()
+    pontos = [ponto.dict()]
+    # TODO continuar e computar as horas it s easy
+
+    res = Ponto.calcular_horas_mes(pontos)
     value = None
     ponto.remover()
     assert res, value
