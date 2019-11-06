71ac12fc8fc29ff3e12d472d2d41c89c98fc4d2f
André Vieira - IVIA <sgavsnake@gmail.com>
2019-11-06 00:40:53 -0300

    removendo pastas desnecessárias do repositório

--- run.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-
-if __name__ == "__main__":
-    from api.api import create_app
-
-    app = create_app('testing')
-    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
--- utils/responser_utils.py
+++
@@ -1,4 +0,0 @@
-def gen_mensagem(mensagem: str, conteudo: dict = None):
-    res = {"mensagem": mensagem}
-    res.update({"conteudo": conteudo} if conteudo else {})
-    return res
